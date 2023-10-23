28a261f57bd8d54991ea160bb90a1859a8f02535
Sana Yoosuf <sanazaier777@gmail.com>
2023-10-23 14:13:19 +0530

    Gym app

---
+++ src/pages/Home.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { Box } from '@mui/material'
+import HeroBanner from '../components/HeroBanner'
+import SearchExercise from '../components/SearchExercise'
+import Exercises from '../components/Exercises'
+
+
+
+const Home = () => {
+  const [bodyPart, setBodyPart] = useState('all')
+  const [exercises, setExercises] = useState([])
+
+  console.log (bodyPart)
+
+  return (
+    <Box>
+      <HeroBanner />
+
+      <SearchExercise
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart} />
+
+      <Exercises
+        setExercises={setExercises}
+        exercises={exercises}
+        bodyPart={bodyPart}
+        />
+    </Box>
+  )
+}
+
+export default Home
